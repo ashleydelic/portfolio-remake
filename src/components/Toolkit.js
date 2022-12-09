c2fab2a1a62a33c1e4882f5e5023aadb2d57556f
ashleydelic <ashleycohzer96@gmail.com>
2022-12-09 13:05:02 -0500

    Update Toolkit.js

--- src/components/Toolkit.js
+++ src/components/Toolkit.js
@@ -13,68 +13,68 @@ const Toolkit = () => {
       id: 1,
       src: <FaHtml5 size={40} />,
       title: "HTML5",
-      style: 'shadow-blue-400 text-start'
+      style: 'text-start'
     },
     {
       id: 2,
       src: <FaCss3Alt size={40} />,
       title: "CSS",
-      style: 'shadow-orange-400 text-start'
+      style: 'text-start'
     },
     {
       id: 3,
       src: <FaJs size={40} />,
       title: "JavaScript",
-      style: 'shadow-orange-400 text-start'
+      style: ''
     },
     {
       id: 4,
       src: <FaReact size={40} />,
       title: "React",
-      style: 'shadow-cyan-200 text-start'
+      style: ' text-start'
     },
     {
       id: 5,
       src: <SiTailwindcss size={40}/>,
       title: "Tailwind CSS",
-      style: 'shadow-cyan-500 text-start'
+      style: 'text-start'
     },
     {
       id: 6,
       src: <FaGithub size={40}/>,
       title: "GitHub",
-      style: "shadow-cyan-400 text-start"
+      style: "text-start"
     },
     {
       id: 7,
       src: <FaFigma size={40}/>,
       title: "Figma",
-      style: "shadow-red-300 text-start"
+      style: "text-start"
     },
     {
       id: 8,
       src: <FaNode size={40}/>,
       title: "Nodejs",
-      style: 'shadow-green-400 text-start'
+      style: 'text-start'
     },
     {
       id: 9,
       src: <SiVisualstudiocode size={40} />,
       title: "Visual Studio Code",
-      style: 'shadow-cyan-400 text-start'
+      style: 'text-center'
     },
     {
         id: 10,
         src: <TbApi size={40}/>,
         title: "REST APIs",
-        style: 'shadow-green-300 text-start'
+        style: 'text-center'
     }
 
   ];
   return (
     <div
       name="toolkit"
-      className=" bg-gradient-to-b from-amber-50 p-12 to-slate-100  w-full h-screen"
+      className=" bg-gradient-to-b from-amber-50 p-12 to-slate-100 w-full h-screen"
     >
       <div className=" max-w-screen-lg  mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
@@ -82,13 +82,13 @@ const Toolkit = () => {
           <p className="py-6 text-gray-500">These are the technologies I've worked with so far...</p>
         </div>
 
-        <div data-AOS="fade-down" data-aos-duration="1500" className="grid grid-cols-3 w-full sm:grid-cols-5 mb-12 gap-8 md:w-full sm:w-full py-6 px-8 ">
+        <div data-AOS="fade-down" data-aos-duration="1500" className="grid grid-cols-3 w-full sm:grid-cols-5 mb-12 gap-8 md:w-full sm:w-full py-6 px-2 ">
           {icons.map(({ id, title, src, style }) => (
-            <div key={id} className={`hover:scale-105 duration-500 flex flex-col justify-evenly p-6 text-amber-600 sm:w-full text-center ${style}`}>
-              <div className=" w-full">
+            <div key={id} className={`hover:scale-105 duration-500 flex flex-row items-center shadow-md justify-evenly p-4 text-amber-600 sm:w-full  text-center ${style}`}>
+              <div className="w-full">
               {src}
               </div>
-            <div className=" text-green-800 text-xs">
+            <div className=" text-green-800 px-2 text-xs">
               {title}
             </div>
             </div>
