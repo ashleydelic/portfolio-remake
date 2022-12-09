78ccd4bedd7f79d13f8a0a706e10a6ab4d7d23a4
ashleydelic <ashleycohzer96@gmail.com>
2022-12-09 13:09:53 -0500

    contact paragraph update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
       <div data-AOS="fade-left" data-aos-duration="1500" className="felx flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="md:text-3xl text-2xl mb-4 font-medium">Let's connect! 🤝</p>
-          <p className="py-6 text-gray-500">I'm interested in junior software developer opportunities, a cup of virtual coffee is a must as well! <br>
+          <p className="py-6 text-gray-500">I'm interested in junior software developer opportunities, a cup of virtual coffee is also a must! <br>
           </br>Feel free to use the form below and we can have a chat, thanks for everything and take care!</p>
         </div>
 
